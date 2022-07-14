44368105a4ef932c4898a138e33b7a79d33140ae
Viktor <viktor.ihtiyarov@gmail.com>
2022-07-14 23:18:58 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)  {
+        System.out.println("Hello SkyPRO!");
+        System.out.println("Задание 1..");
+
+
     }
 }
